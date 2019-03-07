3c471d64e913f5674576d3969310ab537f6ed631
Vadym Surhyk <ironreality@gmail.com>
2019-03-07 12:05:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# A Python script to obtain an ovirt4 vm's IP address 
+
+The script looks for the vm's IP in the specified subnet prefix.
+
+## Parameters
+```
+--api_url - the oVirt's API url
+--api_user - the API's username
+--api_pass - the user's password
+--vm_id - the VM ID
+--subnet_prefix - the oVirt subnet's prefix to look for the VM IP in. Example: "10.60.28."
+```
---
+++ ovirt-vm-get-ip.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+
+import logging
+import sys
+import time
+import re
+from optparse import OptionParser
+
+import ovirtsdk4 as sdk
+
+DEFAULT_API_USER = "admin@internal"
+DEFAULT_SUBNET_PREFIX_PATTERN = "10.60.28."
+
+def parse_args():
+    parser = OptionParser(description='Get the IP of a running VM')
+
+    parser.add_option('--debug', action='store_true', default=False, help='debug mode')
+    parser.add_option('--api_url', default=None, help='oVirt API IP Address/Hostname')
+    parser.add_option( '--api_user', default=DEFAULT_API_USER, help='oVirt API Username, defaults to "%s"' % (DEFAULT_API_USER))
+    parser.add_option('--api_pass', default=None, help='oVirt API Password')
+    parser.add_option('--vm_id', default=None, help='ID of an existing VM to add a disk to')
+    parser.add_option( '--subnet_prefix', default=DEFAULT_SUBNET_PREFIX_PATTERN, help='Subnet the vm\'s IP is belonging to, defaults to "%s"' % (DEFAULT_SUBNET_PREFIX_PATTERN))
+    (opts, args) = parser.parse_args()
+
+    for optname in ["api_url", "api_pass", "api_user", "vm_id"]:
+        optvalue = getattr(opts, optname)
+        if not optvalue:
+            parser.print_help()
+            parser.print_usage()
+            print "Please re-run with an option specified for: '%s'" % (optname)
+            sys.exit(1)
+
+    return opts
+
+def setup_logging(debug=False):
+    if debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+
+if __name__ == "__main__":
+    opts = parse_args()
+    debug = opts.debug
+    setup_logging(debug)
+
+    api_url = opts.api_url
+    api_user = opts.api_user
+    api_pass = opts.api_pass
+    vm_id = opts.vm_id
+    subnet_prefix = opts.subnet_prefix
+
+    connection = sdk.Connection(url=api_url, username=api_user, password=api_pass, insecure=True)
+    if not connection:
+        print "Failed to connect to '%s'" % (url)
+        sys.exit(1)
+
+    vms_service = connection.system_service().vms_service()
+    vm = vms_service.vm_service(vm_id)
+    devices = vm.reported_devices_service().list()
+    if len(devices) > 0:
+
+        vm_ip = None 
+
+        for device in devices:
+            if device.ips:
+                for ip in device.ips:
+                    vm_ip = re.search(subnet_prefix, ip.address)
+                    if vm_ip:
+                        print(ip.address)
+                        connection.close()
+                        sys.exit(0)
+            else:
+                pring("Can't find any IP up for vm '%s' Exiting..." % (vm_id))
+                connection.close()
+                sys.exit(127)
+
+        if vm_ip is None:
+            print("Can't find any active IP matching the target subnet prefix '%s'" % (subnet_prefix))
+            connection.close()
+            sys.exit(127)
+    else:
+        print("Can't find the network interfaces for vm '%s' Exiting..." % (vm_id))
+        connection.close()
+        sys.exit(127)
